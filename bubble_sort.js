ad1221cf8c189c42b51b542502d7cea83467e585
Timur <timengalytchev@mail.ru>
2024-02-06 23:34:19 +0300

    bubble sort

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,22 +1,22 @@
-const array = [1,3,2,7,5,6,0,-4,8,-2,9,-1,10,-5]
+const startArray = [1,3,2,7,5,6,0,-4,8,-2,9,-1,10,-5]
 let count = 0
 
-function bubbleSort(array){
-    for (let i = 0; i < array.length; i++){
+function bubbleSort(inputArray){
+    for (let i = 0; i < inputArray.length; i++){
 
-        for (let j = 0; j < array.length; j++){
-            if(array[j] > array[j + 1]){
-                let tmp = array[j]
-                array[j] = array[j + 1]
-                array[j + 1] = tmp
+        for (let j = 0; j < inputArray.length; j++){
+            if(inputArray[j] > inputArray[j + 1]){
+                let tmp = inputArray[j]
+                inputArray[j] = inputArray[j + 1]
+                inputArray[j + 1] = tmp
             }
             count+=1
         }
     }
-    return array
+    return inputArray
 }
 
-console.log(bubbleSort(array))
+console.log(bubbleSort(startArray))
 console.log(count)
 
 //O(n^2) without coefficient before n^2, then that algo worse than selection dort
